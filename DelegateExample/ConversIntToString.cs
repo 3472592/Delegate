fbbfce3c51979b6f22eac1a6d619fdd068fdef8d
3472592 <3472592@gmail.com>
2024-06-27 05:50:21 -0400

    Add files via upload

---
+++ DelegateExample/ConversIntToString.cs
@@ -0,0 +1,5 @@
+﻿namespace DelegateExample;
+
+//can be used to declare variables, just as You can use
+//a class or interface as type to define variables.
+public delegate string ConvertsIntToString(int i);
---
+++ DelegateExample/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DelegateExample;
+
+public class Program
+{
+    //match method signature to ConvertsIntToString.
+    private static string HiThere(int i)
+    {
+        return "Hi there! #" + (i * 100);
+    }
+
+    private static void Main(string[] args)
+    {
+        // convertation is a var whose type is ConvertsIntToString.
+        // it is a lot like reference variable, except instead of
+        // putting a label on an obj on the heap, putting label on a method.
+        ConvertsIntToString convertation = new(HiThere);
+        // can set just like any other var. When calling it like a method,
+        // it calls whatever method it happens to point to.
+        string msg = convertation(5);
+        Console.WriteLine(msg);
+        Console.ReadKey();
+    }
+
+}
